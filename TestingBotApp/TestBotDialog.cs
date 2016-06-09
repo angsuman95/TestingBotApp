83bbb85ef6641a82bc94148505370f0f85c8563f
angsuman95 <angsuman_ray@yahoo.co.in>
2016-06-09 17:23:02 +0530

    Add project files.

---
+++ TestingBotApp/TestBotDialog.cs
@@ -0,0 +1,63 @@
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingBotApp
+{
+    [Serializable]
+    class TestBotDialog : IDialog<object>
+    {
+        private StartOptions startOptions;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(WelcomeAsync);
+        }
+
+        public async Task WelcomeAsync(IDialogContext context, IAwaitable<Message> argument)
+        {
+            //Message message = await argument;
+            //await context.PostAsync(message.Text);
+
+            PromptDialog.Choice(
+                context: context,
+                resume: AlreadyNewAsync,
+                options: Enum.GetValues(typeof(StartOptions)).Cast<StartOptions>().ToArray(),
+                prompt: "Welcome to VAM Insurance Bot!!",
+                retry: "I didn't understand. Please try again.");
+        }
+
+        public async Task AlreadyNewAsync(IDialogContext context, IAwaitable<StartOptions> argument)
+        {
+            startOptions  = await argument;
+
+            if (startOptions.Equals(StartOptions.Already))
+            {
+                await context.PostAsync("Enter your Policy Number: ");
+                context.Wait(WelcomeAsync);
+            }
+            else
+            {
+                await context.PostAsync("Please visit out website www.abc.com or call on XXXXX for more details." + "\n\n" + "Thank You!!" + "\n\n" + "Have a nice day!");
+                context.Wait(WelcomeAsync);
+            }
+        }
+
+        //public async Task PolicyNumberAsync(IDialogContext context, IAwaitable<object> result)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public enum StartOptions
+        {
+            Already,
+            New
+        }
+
+        private static IList<string> Options = new List<string>();
+    }
+}
